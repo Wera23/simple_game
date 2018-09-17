50b63484bb87633b0d275d1088f5c07e538d9765
Wera Chodanionek <werachodanionek@gmail.com>
2018-09-17 16:54:08 +0200

    fixes in app.js

--- app.js
+++ app.js
@@ -10,6 +10,38 @@ new Vue({
             this.gameIsRunning = true;
             this.powerYou = 100;
             this.powerMonster = 100;
+        },
+        attack: function(){
+            var max = 10;
+            var min = 3;
+            var loss = Math.max(Math.floor(Math.random()* max) + 1, min)
+            this.powerYou -= loss;
+
+            if (this.powerYou <= 0){
+                alert('Potwór przegrał!');
+                this.gameIsRunning = false;
+                return;
+            }
+            
+            var max = 12;
+            var min = 5;
+            var loss = Math.max(Math.floor(Math.random()* max) + 1, min)
+            this.powerMonster -= loss;
+
+            if (this.powerMonster <= 0){
+                alert('Potwór wygrał!');
+                this.gameIsRunning = false;
+                
+            }
+        },
+        specialAttack: function(){
+
+        },
+        addLife: function(){
+
+        },
+        giveUp: function(){
+
         }
     }
 })
