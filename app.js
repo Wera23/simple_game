cd33af6d748ccdba89e52719ccb76d66623e74c6
Wera Chodanionek <werachodanionek@gmail.com>
2018-09-17 17:48:56 +0200

    fixes in js

--- app.js
+++ app.js
@@ -6,42 +6,52 @@ new Vue({
         gameIsRunning: false
     },
     methods: {
-        startGame: function(){
+        startGame: function () {
             this.gameIsRunning = true;
             this.powerYou = 100;
             this.powerMonster = 100;
         },
-        attack: function(){
-            this.powerYou -= this.calculateLoss(2,9);//(min, max)
-            
-            if (this.powerYou <= 0){
-                alert('Wygrałeś!');
-                this.gameIsRunning = false;
+        attack: function () {
+            this.powerMonster -= this.calculateLoss(4, 11);
+            if (this.checkWin()) {
                 return;
-            }     
-                       
-            this.powerMonster -= this.calculateLoss(4,11);           
-
-            if (this.powerMonster <= 0){
-                alert('Potwór wygrał!');
-                this.gameIsRunning = false;
-                
             }
-        },
-        specialAttack: function(){
+            this.powerYou -= this.calculateLoss(2, 9);//(min, max)
+            this.checkWin();
 
         },
-        addLife: function(){
+        specialAttack: function () {
 
         },
-        giveUp: function(){
+        addLife: function () {
 
-        }, 
-        calculateLoss: function(min, max){
-            return Math.max(Math.floor(Math.random()* max) + 1, min);
+        },
+        giveUp: function () {
+
+        },
+        calculateLoss: function (min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+            //(max, min) - max(zaokręglenie z losowej liczby razy max (co da wynik z przecinkiem, ale dzięki floor zaokrągli. +1 jest po to, by gdy padnie o.6 to zaokrąglenie będzie do 0 a nie może być atak na 0. z plus jeden będzie zawsze wyżej od zera))
+        },
+        checkWin: function () {
+            if (this.powerMonster <= 0) {
+                if (confirm('Wygrałeś! Czy chcesz zacząć nową grę?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.powerYou <= 0) {
+                if (confirm('Przegrałeś! Zacząć nową grę?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
-        //(max, min) - max(zaokręglenie z losowej liczby razy max (co da wynik z przecinkiem, ale dzięki floor zaokrągli. +1 jest po to, by gdy padnie o.6 to zaokrąglenie będzie do 0 a nie może być atak na 0. z plus jeden będzie zawsze wyżej od zera))
-       
     }
-})
+
+});
 
